f9b3f6d2418800aee63d7502eed351526b773571
zsuto_000 <zakariasutomo@gmail.com>
2016-06-24 16:01:47 +0700

    detailactivity (belum selesai)

--- app/src/main/java/id/tugasakhir/zakaria/lbsatmastar/DetailActivity.java
+++ app/src/main/java/id/tugasakhir/zakaria/lbsatmastar/DetailActivity.java
@@ -1,17 +1,12 @@
 package id.tugasakhir.zakaria.lbsatmastar;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
